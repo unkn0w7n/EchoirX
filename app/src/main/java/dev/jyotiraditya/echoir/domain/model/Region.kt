771c5c37522b065992d3627d94ab66a47e86d1ea
Jyotiraditya Panda <jyotiraditya@aospa.co>
2024-12-12 15:02:02 +0530

    feat(region): expand regions to match streaming currents
    Signed-off-by: Jyotiraditya Panda <jyotiraditya@aospa.co>

--- app/src/main/java/dev/jyotiraditya/echoir/domain/model/Region.kt
+++ app/src/main/java/dev/jyotiraditya/echoir/domain/model/Region.kt
@@ -1,11 +1,35 @@
 package dev.jyotiraditya.echoir.domain.model
 
 enum class Region(val code: String, val displayName: String) {
+    ALBANIA("AL", "Albania"),
+    ARGENTINA("AR", "Argentina"),
+    AUSTRALIA("AU", "Australia"),
+    AUSTRIA("AT", "Austria"),
+    BELGIUM("BE", "Belgium"),
     BRAZIL("BR", "Brazil"),
-    FINLAND("FI", "Finland"),
+    CANADA("CA", "Canada"),
+    CHILE("CL", "Chile"),
+    COLOMBIA("CO", "Colombia"),
+    DOMINICAN_REPUBLIC("DO", "Dominican Republic"),
+    FRANCE("FR", "France"),
     GERMANY("DE", "Germany"),
+    HONG_KONG("HK", "Hong Kong"),
+    ISRAEL("IL", "Israel"),
+    ITALY("IT", "Italy"),
+    JAMAICA("JM", "Jamaica"),
     MALAYSIA("MY", "Malaysia"),
+    MEXICO("MX", "Mexico"),
     NEW_ZEALAND("NZ", "New Zealand"),
+    NIGERIA("NG", "Nigeria"),
+    PERU("PE", "Peru"),
+    PUERTO_RICO("PR", "Puerto Rico"),
+    SINGAPORE("SG", "Singapore"),
+    SOUTH_AFRICA("ZA", "South Africa"),
+    SPAIN("ES", "Spain"),
+    THAILAND("TH", "Thailand"),
+    UGANDA("UG", "Uganda"),
+    UNITED_ARAB_EMIRATES("AE", "United Arab Emirates"),
+    UNITED_KINGDOM("GB", "United Kingdom"),
     UNITED_STATES("US", "United States");
 
     companion object {
